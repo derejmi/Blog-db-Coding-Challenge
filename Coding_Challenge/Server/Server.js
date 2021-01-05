f8c7ab0822bed84a074cce0915e7522888458827
derejmi <derejemireyigbe@gmail.com>
2021-01-05 17:29:16 +0000

    add seeds

---
+++ Coding_Challenge/Server/Server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const port = 3000;
+
+server.listen(port, () =>
+  console.log(`Express now departing from http://localhost:${port}!`)
+);
+
+// server.use("/blogs", blogRoutes);
+
+//Root route
+server.get("/", (req, res) => res.send("Hi"));
